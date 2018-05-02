35190cf6216a8ecb1aa39d59cab628b73f5ae861
moniqueberry88 <moniqueberry88@gmail.com>
2018-05-01 21:19:17 -0500

    serving the post

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPost: null
+    };
+
+    componentDidMount = () => {
+        axios
+            .get('/posts')
+            .then(Response => {
+                const posts = Response
+                    .data
+                    .slice(0, 4);
+
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Monique'
+                    }
+
+                })
+
+                this.setState({posts: updatedPosts})
+            })
+
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPost: id})
+    }
+
+    render() {
+
+        const posts = this
+            .state
+            .posts
+            .map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPost}/>
+                </section>
+                <section>
+                    <NewPost/>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
